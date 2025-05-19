5198ed3820e2cea62bb17b5ba5a7fd1a205e4478
Mohammadreza <mohammadreza.souri.07@gmail.com>
2025-05-19 05:37:26 +0330

    Add tests for listing ingredients

---
+++ app/recipe/tests/test_ingredient_api.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from core.models import Ingredient
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse("recipe:ingredient-list")
+
+
+def create_user(email="test@gmail.com", password="test1234567"):
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicIngredientsAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_user_is_anonymous(self):
+        response = self.client.get(INGREDIENTS_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsAPITest(TestCase):
+    def setUp(self):
+        self.user = create_user()
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_ingredient(self):
+        Ingredient.objects.create(user=self.user, name="ing")
+
+        response = self.client.get(INGREDIENTS_URL)
+
+        queryset = Ingredient.objects.order_by("-name")
+        serializer = IngredientSerializer(queryset, many=True)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_ingredient_limited_to_user(self):
+        other_user = create_user(email="other@gmail.com", password="test1234")
+        Ingredient.objects.create(user=other_user, name="ing1")
+
+        ingredient2 = Ingredient.objects.create(user=self.user, name="ing2")
+
+        response = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["name"], ingredient2.name)
+        self.assertEqual(response.data[0]["id"], ingredient2.id)
